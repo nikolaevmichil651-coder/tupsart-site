77bcc61130b2118f741b2376084dc3eb158be97a
nikolaevmichil651-coder <nikolaevmichil651@gmail.com>
2026-05-10 09:29:27 -0700

    Create tailwind.config.jsAdd Tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  darkMode: 'class',
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#f97316',
+        },
+      },
+    },
+  },
+  plugins: [],
+};
